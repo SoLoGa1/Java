04acd82670e44cb227c92855a1fe540fdf97c0e7
Revaz Sologhashvili <50874461+SoLoGa1@users.noreply.github.com>
2019-05-22 08:28:19 -0700

    Add files via upload

---
+++ Wrong Subtraction/src/com/Substraction/Main.java
@@ -0,0 +1,33 @@
+package com.Substraction;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+int n;
+int x;
+
+
+System.out.println("input positive integer ");
+n = in.nextInt();
+int tmp =n;
+int tmp2=0;
+System.out.println("how many times do you want to decrease this number by one");
+x = in.nextInt();
+
+
+for (int i=0;i<x;i++) {
+    if (n % 10 != 0) {  // n ის შემოწმება არის თუ არა დამგვალებული ათეულებამდე
+        n = n - 1;
+
+    } else {
+        n = n / 10;
+        tmp2++;
+    }
+}
+        System.out.println(" substraction of "+ tmp + " by "+ x+" times  is "+n);
+        System.out.println( tmp + " was divided by 10 "+ tmp2+" times");
+    }
+}
